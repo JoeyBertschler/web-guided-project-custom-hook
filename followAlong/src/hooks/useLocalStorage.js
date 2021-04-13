d04d5e36c0dad9799614b0486cf6d0048634fc34
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-12 18:27:53 -0700

    lecture work-in-progress

--- followAlong/src/hooks/useLocalStorage.js
+++ followAlong/src/hooks/useLocalStorage.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 // An upgraded version of useState, with the same interface but the added functionality of reading to / writing from localStorage
-export const useLocalStorage = () => {
-
+export const useLocalStorage = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  return [value, setValue];
 }
