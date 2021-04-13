663e8a3b77ac9c8849af1e2d50b2c91b9ddbfd8f
Josh Jacobson <joshjacobson14@gmail.com>
2021-04-12 17:50:40 -0700

    lecture work-in-progress

--- followAlong/src/hooks/useForm.js
+++ followAlong/src/hooks/useForm.js
@@ -8,5 +8,9 @@ export const useForm = () => {
     setFirstName(e.target.value);
   };
 
-  return [firstName, setFirstName, handleChanges];
+  return [handleChanges, firstName, setFirstName];
 }
+
+// We can define any return format we like. Using an array and dereferencing each attribute on the component side is a stylistic choice, which many developers like because it reminds us of the built-in hooks. Do it however you like, but just make sure that your returns here match the dereferencing in your component! It's easy to get out of sync while working on custom hooks :D
+
+// Remember: order matters when dereferencing from an array! (not from an object)
